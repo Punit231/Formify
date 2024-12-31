4be1c4e606de4b0817509da000334f39a05c25fe
Punit Parmar <punitparmar231@gmail.com>
2024-12-31 19:34:16 +0530

    update changes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "5mb", // Allow up to 5MB in request body
+      allowedOrigins: ["http://localhost:3000", ""], // Allow localhost (development) and production domain
+    },
+  },
 };
 
 export default nextConfig;
