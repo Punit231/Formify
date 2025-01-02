3a6134890555ccc5eaf92bbd8d9dc269898b3fc1
Punit Parmar <punitparmar231@gmail.com>
2025-01-02 09:23:11 +0530

    form action updated

--- actions/form.ts
+++ actions/form.ts
@@ -63,3 +63,19 @@ export async function CreateForm(data: formSchemaType) {
 
     return form.id;
 }
+
+export async function GetForms() {
+    const user = await currentUser();
+    if (!user) {
+        throw new UserNotFoundErr()
+    }
+
+    return prisma.form.findMany({
+        where: {
+            userId: user.id,
+        },
+        orderBy: {
+            createdAt: "desc"
+        }
+    })
+}
