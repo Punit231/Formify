28dc125222c77ef05af77afdd60d11d085f53b97
Punit Parmar <punitparmar231@gmail.com>
2025-01-02 09:49:59 +0530

    config update

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,17 @@ const nextConfig: NextConfig = {
       bodySizeLimit: "5mb", // Allow up to 5MB in request body
       allowedOrigins: ["http://localhost:3000", ""], // Allow localhost (development) and production domain
     },
+    turbo: {},
   },
 };
+
+// module.exports = {
+//   experimental: {
+//       appDir: true,
+//   },
+//   webpack: (config: any) => {
+//       return config;
+//   },
+// };
 
 export default nextConfig;
